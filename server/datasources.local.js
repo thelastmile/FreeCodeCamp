22bf2471bc918936dcd67aba2a43dd2126d8aa83
Carl Chen <carlchen214@gmail.com>
2016-09-27 00:19:15 -0700

    add email according to kyles instructions

--- server/datasources.local.js
+++ server/datasources.local.js
@@ -6,12 +6,29 @@ module.exports = {
     connectionTimeout: 10000,
     url: secrets.db
   },
-  mail: {
-    connector: 'mail',
-    transport: {
-      type: 'ses',
-      accessKeyId: process.env.SES_ID,
-      secretAccessKey: process.env.SES_SECRET
+  // mail: {
+  //   connector: 'mail',
+  //   transport: {
+  //     type: 'ses',
+  //     accessKeyId: process.env.SES_ID,
+  //     secretAccessKey: process.env.SES_SECRET
+  //   }
+  // }
+  "mail": {
+        "name": "mail",
+        "defaultForType": "mail",
+        "connector": "mail",
+        "transports": [
+            {
+                "type": "SMTP",
+                "host": "mail.tlmworks.org",
+                "secure": true,
+                "port": 465,
+                "auth": {
+                    "user": "freecodecamp@tlmworks.org",
+                    "pass": "{{ password here }}"
+                }
+            }
+        ]
     }
-  }
 };
